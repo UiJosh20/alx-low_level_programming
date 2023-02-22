bb5c3969121ef23a93a8dba04a178907965e77c3
joshua <adeyeriseun0@gmail.com>
2023-02-22 06:52:17 -0800

    print _purchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
- * Description: 'program that prints _putchar and a new line'
  *
- * return: Always 0 (Success)
+ * Return: Always 0
  *
  */
 int main(void)
@@ -18,5 +18,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
