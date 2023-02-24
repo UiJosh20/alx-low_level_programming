342af8089866adc6116f0b3f6da8bbfb16929309
joshua <adeyeriseun0@gmail.com>
2023-02-24 09:47:09 -0800

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	for (; c <= 9; c++)
+	{
+	if (c == 2 || c == 4)
+	{
+		continue;
+	}
+	else
+	{
+		_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
+}
