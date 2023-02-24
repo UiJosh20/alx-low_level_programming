644e9edc4c2ea7905a87198eb02e557dfe729018
joshua <adeyeriseun0@gmail.com>
2023-02-24 09:34:34 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	void print_numbers(void);
+	print_numbers();
 	return (0);
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * print_numbers - Print the numbers from 0 to 9
  *
- * Return: The numbers from 0 to 9
+ * Return: Void
  */
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 0; c < 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
 		_putchar(c + '0');
 	}
