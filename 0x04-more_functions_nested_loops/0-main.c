c73ea178646b35f462205c7c938019a9960fef3b
joshua <adeyeriseun0@gmail.com>
2023-02-24 05:42:11 -0800

    detect uppercase

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
 /**
  * main - check the code.
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    char c;
-
-    c = 'A';
-    printf("%c: %d\n", c, isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	char c;
+	c = 'A';
+	printf("%c: %d\n", c, isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, isupper(c));
+	return (0);
 }
