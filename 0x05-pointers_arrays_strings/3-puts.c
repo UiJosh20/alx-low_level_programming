09e76a65e216a75e6d24c92b08670b900c62d104
joshua <adeyeriseun0@gmail.com>
2023-02-27 06:56:32 -0800

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,9 +6,9 @@
  * @str: string to print
  * Return: void
  */
-void _puts(char *str)tr
+void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	for (; *str  != '\0'; str++)
 	{
 		_putchar(*str);
 	}
