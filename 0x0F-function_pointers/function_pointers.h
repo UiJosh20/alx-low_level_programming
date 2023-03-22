126fd59a0821dd0d7c8da459c891afe49d934948
joshua <adeyeriseun0@gmail.com>
2023-03-22 03:49:57 -0700

    FUNCTION POINER: array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,10 @@
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void array_iterator(int *array, size_t size, void (*action)(int));
+void print_elem(int elem);
+void print_elem_hex(int elem);
