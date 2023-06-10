9ee092ae82c139bf0b0a7d5f818ea3c33ef58beb
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-10 05:51:20 -0700

    prints programe name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - prints it's name followed by a newline
  * @argc: argument count
  * @argv: argument vector
  * Return: always 0
  */
-int main(innt argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
