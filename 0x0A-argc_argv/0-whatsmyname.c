4377ceca4c78080394de68df930546e7325f81e1
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-10 05:39:51 -0700

    Readme & Program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints it's name followed by a newline
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(innt argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
