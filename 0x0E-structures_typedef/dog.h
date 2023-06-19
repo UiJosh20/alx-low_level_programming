177795aa41a2432350769c251fe7c7432a407e4d
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-19 05:09:23 -0700

    updated headerfile

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
