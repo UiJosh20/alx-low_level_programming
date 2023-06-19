c78454afdef31a7cc53eb94491e04d1b543f0a8e
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-19 05:44:30 -0700

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,9 +18,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = name;
+	new_dog->name = malloc((strlen(name) + 1) * sizeof(char));
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = malloc((strlen(owner) + 1) * sizeof(char));
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->name, name);
+	new_dog->age = age;
+	strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
 
