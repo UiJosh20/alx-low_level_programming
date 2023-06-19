d74e854659d78946cc64d39c8fe1c167ba666782
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-19 05:56:08 -0700

    initialize a variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = (char *)malloc((strlen(name) + 1) * sizeof(char));
-	d->age = age;
 	d->owner = (char *)malloc((strlen(owner) + 1) * sizeof(char));
 
 	strcpy(d->name, name);
