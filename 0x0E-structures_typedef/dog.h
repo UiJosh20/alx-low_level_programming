60cc0af9fbf4878fc6521decbcbedd1a1ad8ec89
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-19 03:54:18 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - struct name
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: nothing
+ */
 struct dog
 {
 	char *name;
