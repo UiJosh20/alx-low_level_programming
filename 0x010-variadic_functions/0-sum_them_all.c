644790fed5d26f435929cf879d735cfee067ff39
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-22 03:39:44 -0700

    sum them all

---
+++ 0x010-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums all it parameter
+ * @n: an integer;
+ * Return: the sum of all its parameter
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int result;
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(args, n);
+
+	i = 0;
+	while (i < n)
+	{
+		result += va_arg(args, int);
+		i++;
+	}
+	va_end(args);
+	return (result);
+}
