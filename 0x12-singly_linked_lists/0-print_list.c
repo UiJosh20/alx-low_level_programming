208f2c624691e545f9f0acd16eeaed7fafded4ab
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-04 04:05:54 -0700

    headerfile and print_list file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * print_list - prints all the element of a list
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	printf("[");
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("%s", h->str);
+		}
+		else
+		{
+			printf("[0] (nil)");
+		}
+		count++;
+		h = h->next;
+		if (h != NULL)
+		{
+			printf(", ");
+		}
+	
+	}
+	printf("]\n");
+	return (count);
+}
