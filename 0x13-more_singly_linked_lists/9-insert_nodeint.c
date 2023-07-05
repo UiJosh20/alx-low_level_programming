680a15854a4f56c018cb4c8aebc47f9abdf3e4b8
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-05 11:38:17 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-
 	if (idx == 0)
 	{
 		new_node = malloc(sizeof(listint_t));
@@ -29,28 +28,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
 	current = *head;
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
 	}
-
 	if (current == NULL)
 	{
 		return (NULL);
 	}
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
 	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
-
 	return (new_node);
 }
