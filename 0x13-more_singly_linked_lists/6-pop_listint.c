6b2518d2c68acc45685e33099476b5a8ae60e6d5
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-05 09:55:55 -0700

    pop a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - deletes the head of a list
+ * @head; pointer to the head
+ * Return: and integer
+ */
+int pop_listint(listint_t **head)
+{
+	int data = 0;
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
+	{
+		return (0);
+	}
+	temp = *head;
+	data = temp->n;
+	*head = (*head)->next;
+	free(temp);
+
+	return (data);
+}
