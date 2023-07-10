2604b7e5bf7baac5512e0142deeeb63b9d9f0856
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-10 05:20:17 -0700

    read a textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to stdout
+ * @filename: the file
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer = malloc(letters + 1);
+	FILE *file = fopen(filename, "r");
+	ssize_t bytes_read = fread(buffer, sizeof(char), letters, file);
+	ssize_t i;
+
+	if (file == NULL)
+	{
+		return (0);
+	}
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	if (bytes_read < 0)
+	{
+		fclose(file);
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+	i = 0;
+	while (i < bytes_read)
+	{
+		_putchar(buffer[i]);
+		i++;
+	}
+
+	fclose(file);
+	free(buffer);
+	return (bytes_read);
+
+}
