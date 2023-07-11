07c836438e28517117ea3df0d159c28c4e8e8e1b
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-07-11 03:52:52 -0700

    update on file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,14 +16,20 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	file = open(filename, O_APPEND | O_WRONLY);
 	if (file == -1)
+	{
 		return (-1);
+	}
 
 	for (i = 0; text_content[i] != '\0'; i++)
+	{
 		;
+	}
 
 	written = write(file, text_content, i);
 	if (written == -1)
+	{
 		return (-1);
+	}
 
 	close(file);
 	return (1);
