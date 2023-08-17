8aa737d188bdb621a6a4c3126c19ca1a3346c023
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-17 12:25:00 -0700

    deletes a node

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -27,13 +27,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	while (i < index)
 	{
 		if (current == NULL)
-		{
 			return (-1);
-		}
 		current = current->next;
 		i++;
 	}
-
 	if (current == NULL)
 	{
 		return (-1);
