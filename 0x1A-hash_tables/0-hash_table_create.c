b343a2d06008b7b537d9ef44fbc4b16378aa7cb6
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-09-11 02:55:05 -0700

    hash table header file and first file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable;
+	unsigned long int j;
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+	hashtable->size = size;
+	hashtable->array = malloc(size * sizeof(hash_node_t *));
+	if (hashtable->array == NULL)
+	{
+		free(hashtable);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+	return (hashtable);
+}
